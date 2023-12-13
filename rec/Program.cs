5fbf96bbf1f4f61d4e825b681701861346324f18
cj3016595 <cj3016595@LAB01-PC09>
2023-12-13 12:28:19 -0300

    Adicionar arquivos de projeto.

---
+++ rec/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, n1;
+            Console.WriteLine("2,3,5,7");
+           int.Parse(Console.ReadLine());
+            n =  1/ 1;
+            n1 = n / n;
+           
+            
+            Console.ReadKey();
+           
+    }
+    }
+}
